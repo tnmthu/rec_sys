20e551a31e5a183a66329cd0d9f1490ea76b4750
tnmthu <tnmthu.maria98@gmail.com>
2020-06-13 13:14:03 +0700

    fix: warning

--- src/components/Header/Dropdown.js
+++ src/components/Header/Dropdown.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Menu, Dropdown } from "antd";
 
 const DropdownContainer = (props) => {
@@ -7,7 +8,7 @@ const DropdownContainer = (props) => {
       {props.subMenu.map((item) => {
         return (
           <Menu.Item>
-            <a>{item}</a>
+            <Link to="#">{item}</Link>
           </Menu.Item>
         );
       })}
